ba05b9ec2a023af2e7360e85b2d249014e6b1cdf
Aiden <bokjaeoh@gmail.com>
2022-03-07 21:42:13 +0900

    Add tests for `parseOrder`

---
+++ test/parseOrder.test.js
@@ -0,0 +1,52 @@
+const { parseOrder } = require('../src')
+
+
+// undefined
+test('parseOrder() returns \'\'', () => {
+  expect(parseOrder()).toBe('')
+})
+
+// null
+test('parseOrder(null) returns \'\'', () => {
+  expect(parseOrder(null)).toBe('')
+})
+
+// ''
+test('parseOrder(\'\') returns \'\'', () => {
+  expect(parseOrder('')).toBe('')
+})
+
+// []
+test('parseOrder([]) returns \'\'', () => {
+  expect(parseOrder([])).toBe('')
+})
+
+// 'age'
+test('parseOrder(\'age\') returns \' ORDER BY age\'', () => {
+  expect(parseOrder('age')).toBe(' ORDER BY age')
+})
+
+// 'dateReg DESC'
+test('parseOrder(\'dateReg DESC\') returns \' ORDER BY dateReg DESC\'', () => {
+  expect(parseOrder('dateReg DESC')).toBe(' ORDER BY dateReg DESC')
+})
+
+// 'dateReg DESC, age'
+test('parseOrder(\'dateReg DESC, age\') returns \' ORDER BY dateReg DESC, age\'', () => {
+  expect(parseOrder('dateReg DESC, age')).toBe(' ORDER BY dateReg DESC, age')
+})
+
+// ['age']
+test('parseOrder([\'age\']) returns \' ORDER BY age\'', () => {
+  expect(parseOrder(['age'])).toBe(' ORDER BY age')
+})
+
+// ['dateReg DESC']
+test('parseOrder([\'dateReg DESC\']) returns \' ORDER BY dateReg DESC\'', () => {
+  expect(parseOrder(['dateReg DESC'])).toBe(' ORDER BY dateReg DESC')
+})
+
+// ['dateReg DESC', 'age']
+test('parseOrder([\'dateReg DESC\', \'age\']) returns \' ORDER BY dateReg DESC, age\'', () => {
+  expect(parseOrder(['dateReg DESC', 'age'])).toBe(' ORDER BY dateReg DESC, age')
+})
